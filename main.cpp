2d53dc07b37cca1a42dfebeba7cd2f265470eab2
hhxc2024 <2269075459@qq.com>
2024-10-07 20:32:41 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,49 +4,4 @@
 void CompareThreeNumbers(int a, int b, int c, Result &result)
 {
     // 在这里完成你的代码
-     int min, mid, max;
-    if(a <= b && a <= c) 
-    {
-        min = a;
-        if(b <= c)
-        {
-            mid = b;
-            max = c;
-        } 
-        else
-        {
-            mid = c;
-            max = b;
-        }
-    }
-    else if(b <= a && b <= c)
-    {
-        mid = b;
-        if(a <= c)
-        {
-            min = a;
-            max = c;
-        } else 
-        {
-            min = c;
-            max = a;
-        }
-    }
-    else 
-    {
-        mid = c;
-        if(a <= b)
-        {
-            min = a;
-            max = b;
-        }
-        else 
-        {
-            min = b;
-            max = a;
-        }
-    }
-    result.min = min;
-    result.mid = mid;
-    result.max = max;
 }
