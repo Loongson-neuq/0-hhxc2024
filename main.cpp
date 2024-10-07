dad3264bc5b35f3f518270fddf5751399d5c92c3
hhxc2024 <2269075459@qq.com>
2024-10-07 21:27:12 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,39 @@
 #include ".assignment/test.h"
 
+void swap(int &lhs,int &rhs)
+{
+    int t = lhs;
+    lhs = rhs;
+    rhs =t;
+}
+
 // 比较三个数的大小
 void CompareThreeNumbers(int a, int b, int c, Result &result)
 {
     // 在这里完成你的代码
+     if(a>b)
+    {
+        swap(a,b)
+
+        if(b>c)
+        {
+            swap(b,c)
+
+        }
+        if(a>b)
+        {
+            swap(a,b)
+        }
+    }
+     else if (b > c) 
+    {
+        swap(b, c);
+        if(a > b)
+        {
+            swap(a, b);
+        }
+    }
+    result.min = a;
+    result.mid = b;
+    result.max = c; 
 }
